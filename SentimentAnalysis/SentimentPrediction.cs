b8f331a9ae702a75bd5f71423e5dafc29af0050f
VCR01 <126965254+VCR01@users.noreply.github.com>
2023-08-27 14:21:17 +0100

    Initial Commit Repair
    Repaired the folder

--- SentimentAnalysis/SentimentPrediction.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.ML.Data;
-
-public class SentimentPrediction
-{
-    [ColumnName("PredictedLabel")]
-    public bool Prediction;
-}
---
+++ SentimentPrediction.cs
@@ -0,0 +1,7 @@
+using Microsoft.ML.Data;
+
+public class SentimentPrediction
+{
+    [ColumnName("PredictedLabel")]
+    public bool Prediction;
+}
